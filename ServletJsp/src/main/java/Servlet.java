1f16f63655f265dd0e9f6bf478d1d36cf7964028
JoTerrance <gorka.zgz@gmail.com>
2016-09-27 12:51:29 +0200

    scaffold de servlet jsp

---
+++ ServletJsp/src/main/java/Servlet.java
@@ -0,0 +1,27 @@
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Servlet extends HttpServlet{
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		redirect(resp);
+		super.doPost(req, resp);
+	}
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		redirect(resp);
+		super.doGet(req, resp);
+	}
+	
+
+	private void redirect(HttpServletResponse resp) throws IOException {
+		resp.sendRedirect("fin.jsp");
+	}
+	
+}

6af28e515b7f12c86f0b7a1c0329e6978e21042b
JoTerrance <gorka.zgz@gmail.com>
2016-10-21 09:14:06 +0200

    added class repository

--- ServletJsp/src/main/java/es/salesianos/service/Service.java
+++ ServletJsp/src/main/java/es/salesianos/service/Service.java
@@ -9,6 +9,7 @@ import es.salesianos.connection.ConnectionH2;
 import es.salesianos.connection.ConnectionManager;
 import es.salesianos.model.User;
 import es.salesianos.model.assembler.UserAssembler;
+import es.salesianos.repository.Repository;
 import es.salesianos.utils.DateConverter;
 
 public class Service {
@@ -22,9 +23,14 @@ public class Service {
 		return UserAssembler.assembleUserFrom(req);
 	}
 
-	public void insertOrUpdate(User user) {
-		Connection conn = manager.open("jdbc:h2:file:./src/main/resources/test");
-		manager.close(conn);
+	public void insertOrUpdate(User userFormulario) {
+		Repository repository = new Repository();
+		User userInDatabase = repository.search(userFormulario);
+		if(null == userInDatabase){
+			repository.insert(userFormulario);
+		}else{
+			repository.update(userFormulario);
+		}
 	}
 
 	public void calculateAgeAndAddIntoRequest(HttpServletRequest req, Date date) {

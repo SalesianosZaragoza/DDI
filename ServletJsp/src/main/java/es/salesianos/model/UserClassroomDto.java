f558c4d8dfa3bf9c143f0241b65a56bd79354a5e
JoTerrance <gorka.sanz@salesianos.edu>
2020-11-17 13:48:42 +0100

    added DTO objects

---
+++ ServletJsp/src/main/java/es/salesianos/model/UserClassroomDto.java
@@ -0,0 +1,20 @@
+package es.salesianos.model;
+
+public class UserClassroomDto {
+	
+	private String userName;
+	private String classroomName;
+	public String getClassroomName() {
+		return classroomName;
+	}
+	public void setClassroomName(String classroomName) {
+		this.classroomName = classroomName;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+}

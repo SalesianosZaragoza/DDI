9eb76802027204e1e4572e2905f678dedd014fc0
Gorka Sanz <gorka.sanz@salesianos.edu>
2022-10-06 09:27:47 +0200

    adapting servlet from xml to annotation

--- JspJstlServlet/src/main/java/es/salesianos/servlet/InsertDataServlet.java
+++ JspJstlServlet/src/main/java/es/salesianos/servlet/InsertDataServlet.java
@@ -8,9 +8,10 @@ import java.util.Optional;
 import es.salesianos.model.Activity;
 import es.salesianos.repository.ActividadesRepository;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-
+@WebServlet("/insert")
 public class InsertDataServlet extends BaseServlet{
 
 	ActividadesRepository repository = new ActividadesRepository();
